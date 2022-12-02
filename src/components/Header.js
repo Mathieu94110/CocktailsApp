905b9384dc33339aa69bf8c57eaf7d7a6ccb217a
MathieuEnault <enault.mathieu2@gmail.com>
2022-12-02 13:36:05 +0100

    adding responsive and fix fontawesome link issue

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,20 +1,21 @@
 import React from 'react';
 import styles from './Header.module.scss';
 import cocktailsImg from '../assets/images/cocktails-logo.png';
+
 function Header() {
   return (
     <header className={`${styles.header} d-flex flex-row align-items-center`}>
       <i className="fa-solid fa-bars"></i>
-      <div className="flex-fill">
+      <div className="flex-fill-vertical-center">
         <img src={cocktailsImg} alt="logo cocktails" />
         <h1>Cocktails Master</h1>
       </div>
-      <ul>
-        <button className="mr-5 btn btn-reverse-primary">
+      <ul className="d-flex">
+        <button className="mr-5 btn btn-reverse-primary header-buttons">
           <i className="fa-solid fa-basket-shopping mr-5"></i>
           <span>Favoris</span>
         </button>
-        <button className="btn btn-primary">Connexion</button>
+        <button className="btn btn-primary header-buttons">Connexion</button>
       </ul>
     </header>
   );
--- src/components/Recipe.js
+++ src/components/Recipe.js
@@ -1,16 +1,24 @@
-import React from 'react';
-import styles from './Recipe.module.scss';
+import React, { useState } from 'react';
 import testCocktail from '../assets/images/test-cocktail.png';
+import styles from './Recipe.module.scss';
+
 function Recipe() {
+  const [liked, setLiked] = useState(false);
+
+  function handleClick() {
+    setLiked(!liked);
+  }
+
   return (
-    <div className={styles.recipe}>
+    <div onClick={handleClick} className={styles.recipe}>
       <div className={styles.imageContainer}>
-        <img src={testCocktail} alt="recipe" />
+        <img src={testCocktail} alt="test-image" />
       </div>
       <div
-        className={`${styles.recipeTitle} d-flex flex-row justify-content-center align-items-center`}
+        className={`${styles.recipeTitle} d-flex flex-column justify-content-center align-items-center`}
       >
-        <h3>Sex and the beach</h3>
+        <h3 className="mb-10">Sex and the beach</h3>
+        <i className={`fa-solid fa-heart ${liked ? 'text-primary' : ''}`}></i>
       </div>
     </div>
   );
