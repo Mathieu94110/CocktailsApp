acd126881aee29df04a3ff06ae7014c9c4b26fa6
MathieuEnault <enault.mathieu2@gmail.com>
2022-12-06 18:50:37 +0100

    adding responsive on recipe page

--- src/router.tsx
+++ src/router.tsx
@@ -29,24 +29,3 @@ export const router = createBrowserRouter([
     ],
   },
 ]);
-// // import React, { lazy } from 'react';
-// import { createBrowserRouter } from 'react-router-dom';
-// import App from './App';
-// import CocktailsRecipeCard from './pages/CocktailsRecipe/CocktailsRecipe';
-
-// // const HomePage = lazy(() => import('./pages/HomePage/HomePage'));
-
-// export const router = createBrowserRouter([
-//   {
-//     path: '/',
-//     element: <App />,
-//   },
-//   {
-//     path: '/recipe',
-//     element: <CocktailsRecipeCard />,
-//   },
-//   {
-//     path: '/recipe/:id',
-//     element: <CocktailsRecipeCard />,
-//   },
-// ]);
