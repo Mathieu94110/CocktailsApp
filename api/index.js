b46cdf2cd3d93626f40b5714c199d81af047fcc2
Mathieu Enault <me94110@gmail.com>
2023-03-17 19:00:44 +0100

    predeploy

--- api/index.js
+++ api/index.js
@@ -1,17 +1,18 @@
-const express = require('express');
-const cookie = require('cookie-parser');
+const express = require("express");
+const cookie = require("cookie-parser");
 
 const app = express();
-const routes = require('./routes');
+const routes = require("./routes");
+app.use(express.static(`${__dirname}/../client/build`));
 app.use(cookie());
 app.use(express.json());
 
-require('./database');
+require("./database");
 
 app.use(routes);
 
-app.use('*', (req, res) => {
-  res.status(404).json('mauvaise routes');
+app.use("*", (req, res) => {
+  res.status(404).json("mauvaise routes");
 });
 
 app.listen(3001);
