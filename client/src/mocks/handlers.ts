0b2131641f0e131cd4a48425170a46e152a2d64d
Mathieu Enault <me94110@gmail.com>
2023-03-18 16:53:04 +0100

    change setupProxy port on client

--- client/src/mocks/handlers.ts
+++ client/src/mocks/handlers.ts
@@ -1,8 +1,6 @@
 import { rest } from 'msw';
 
 export const handlers = [
-  // rest.post('http://localhost:1234/a', (req, res, ctx) =>  res(ctx.text('a'))),
-  // rest.post('http://localhost:1234/x', (req, res, ctx) =>  res(ctx.text('hi'))),
   rest.get('/api/favorites/getFavoredCocktail/:userInfos', (req, res, ctx) => {
     const { userInfos } = req.params;
     return res(
@@ -12,22 +10,19 @@ export const handlers = [
           userFrom: userInfos,
           idDrink: '11007',
           strDrink: 'Margarita',
-          strDrinkThumb:
-            'https://www.thecocktaildb.com/images/media/drink/5noda61589575158.jpg',
+          strDrinkThumb: 'https://www.thecocktaildb.com/images/media/drink/5noda61589575158.jpg',
         },
         {
           userFrom: userInfos,
           idDrink: '14008',
           strDrink: 'Victor',
-          strDrinkThumb:
-            'https://www.thecocktaildb.com/images/media/drink/5noda61589575158.jpg',
+          strDrinkThumb: 'https://www.thecocktaildb.com/images/media/drink/5noda61589575158.jpg',
         },
         {
           userFrom: userInfos,
           idDrink: '12007',
           strDrink: 'Tequila punch',
-          strDrinkThumb:
-            'https://www.thecocktaildb.com/images/media/drink/56oda6158957789f.jpg',
+          strDrinkThumb: 'https://www.thecocktaildb.com/images/media/drink/56oda6158957789f.jpg',
         },
       ])
     );
--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -4,7 +4,7 @@ module.exports = (app) => {
   app.use(
     '/api/*',
     createProxyMiddleware({
-      target: 'http://127.0.0.1:3001',
+      target: 'http://127.0.0.1:80',
       secure: false,
     })
   );
