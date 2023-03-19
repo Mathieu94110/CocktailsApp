c1bb2861b64c31e3c3127bb108c49e653707d6a2
Mathieu Enault <me94110@gmail.com>
2023-03-19 21:49:21 +0100

    adding new database for test

--- api/database/index.js
+++ api/database/index.js
@@ -1,12 +1,14 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-mongoose.set("strictQuery", true)
+mongoose
+  .set("strictQuery", true)
   .connect(
-    'mongodb+srv://byby94110:byby94110@cluster0.byrbrs1.mongodb.net/?retryWrites=true&w=majority'
+    "mongodb+srv://me94110:me94110@cluster0.mtuknxk.mongodb.net/cocktailsTest?retryWrites=true&w=majority"
   )
+
   .then(() => {
-    console.log('CONNEXION DB OK !');
+    console.log("CONNEXION DB OK !");
   })
   .catch((e) => {
-    console.log('CONNEXION KO !', e);
+    console.log("CONNEXION KO !", e);
   });
